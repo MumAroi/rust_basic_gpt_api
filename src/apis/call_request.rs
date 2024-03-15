d3eadb22043f41b1fb3b1499880c05b78edfdc05
paramas-livinginsider <paramas.w@livinginsider.com>
2024-03-15 15:15:45 +0700

    feat: error handling

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,9 +1,12 @@
-use crate::models::general::llm::{Message, ChatCompletion};
+use crate::models::general::llm::{APIResponse, ChatCompletion, Message};
 use dotenv::dotenv;
-use reqwest::{Client, header::{HeaderMap, HeaderValue}};
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Client,
+};
 use std::env;
 
-pub async fn call_gpt(messages: Vec<Message>) {
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
     dotenv().ok();
 
     let api_key: String = env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found in env");
@@ -15,40 +18,68 @@ pub async fn call_gpt(messages: Vec<Message>) {
 
     headers.insert(
         "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
     );
 
     headers.insert(
         "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap()
+        HeaderValue::from_str(api_org.as_str())
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
     );
 
-
-    let client: Client = Client::builder().default_headers(headers).build().unwrap();
+    let client: Client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
     let chat_completion: ChatCompletion = ChatCompletion {
         model: "gpt-4".to_string(),
         messages,
-        temperature: 0.1
+        temperature: 0.1,
     };
 
+    // let res_raw = client
+    //     .post(url)
+    //     .json(&chat_completion)
+    //     .send()
+    //     .await
+    //     .unwrap();
 
-    let res_raw  = client.post(url).json(&chat_completion).send().await.unwrap();
+    // dbg!(res_raw.text().await.unwrap());
 
-    dbg!(res_raw.text().await.unwrap());
+    let res: APIResponse = client
+        .post(url)
+        .json(&chat_completion)
+        .send()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?
+        .json()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
+
+    Ok(res.choices[0].message.content.clone())
 }
 
-
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
 
     #[tokio::test]
     async fn test_call_gpt() {
         let messages: Vec<Message> = vec![Message {
             role: "user".to_string(),
-            content: "Hello, how are you?. Give me a short response.".to_string()
+            content: "Hello, how are you?. Give me a short response.".to_string(),
         }];
-        call_gpt(messages).await;
+        let res = call_gpt(messages).await;
+        match res {
+            Ok(res_str) => {
+                dbg!(res_str);
+                assert!(true);
+            }
+            Err(_) => {
+                assert!(false)
+            }
+        }
     }
 }
