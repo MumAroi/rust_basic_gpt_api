f2afb1290605899ec0cfc7737de22e59c11a1cbe
LookNow <paramas.wae.th@gmail.com>
2024-03-15 19:54:04 +0700

    refactor:  test ai

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,4 +1,5 @@
-use crate::models::general::llm::Message;
+use super::command_line::PrintCommand;
+use crate::{apis::call_request::call_gpt, models::general::llm::Message};
 
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
     let ai_function_str = ai_func(func_input);
@@ -18,6 +19,27 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
     }
 }
 
+pub async fn ai_task_request(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+    let extended_msg: Message = extend_ai_function(function_pass, &msg_context);
+
+    PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+    let llm_response_res: Result<String, Box<dyn std::error::Error + Send>> =
+        call_gpt(vec![extended_msg.clone()]).await;
+
+    match llm_response_res {
+        Ok(llm_resp) => llm_resp,
+        Err(_) => call_gpt(vec![extended_msg.clone()])
+            .await
+            .expect("Failed twice to call GPT"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,4 +52,18 @@ mod tests {
         dbg!(&extended_msg);
         assert_eq!(extended_msg.role, "system".to_string());
     }
+
+    #[tokio::test]
+    async fn tests_ai_task_request() {
+        let ai_func_param: String =
+            "Build me a webserver for making stock price api request.".to_string();
+        let res = ai_task_request(
+            ai_func_param,
+            "Managing Agent",
+            "Defining user requirements",
+            convert_user_input_to_goal,
+        ).await;
+        dbg!(res);
+    }
+
 }
